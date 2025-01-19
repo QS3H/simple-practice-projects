828b6874f607a3815a7f2f7069361d1d97633b7a
shawky <mastershawky2017@gmail.com>
2025-01-19 21:09:09 +0200

    second commit

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,4 +1,14 @@
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
-let currentTime = new Date();
+
+setInterval(() => {
+  let currentTime = new Date();
+  hours.innerHTML =
+    (currentTime.getHours() % 12 < 10 ? "0" : "") +
+    (currentTime.getHours() % 12);
+  minutes.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  seconds.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000);
