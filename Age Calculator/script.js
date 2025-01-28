fa574771d1eed7e0006b146aa92e7bcb588c9bb8
shawky <mastershawky2017@gmail.com>
2025-01-28 21:16:43 +0200

    optimize script

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,44 +1,28 @@
-let userInput = document.getElementById("date");
+const userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
-let result = document.getElementById("result");
+const result = document.getElementById("result");
 
 function calculateAge() {
-  let birthDate = new Date(userInput.value);
+  const birthDate = new Date(userInput.value);
+  const today = new Date();
 
-  let day1 = birthDate.getDate();
-  let month1 = birthDate.getMonth() + 1;
-  let year1 = birthDate.getFullYear();
+  let year3 = today.getFullYear() - birthDate.getFullYear();
+  let month3 = today.getMonth() - birthDate.getMonth();
+  let day3 = today.getDate() - birthDate.getDate();
 
-  let today = new Date();
-
-  let day2 = today.getDate();
-  let month2 = today.getMonth() + 1;
-  let year2 = today.getFullYear();
-
-  let day3, month3, year3;
-
-  year3 = year2 - year1;
-
-  if (month2 >= month1) {
-    month3 = month2 - month1;
-  } else {
-    year3--;
-    month3 = 12 + month2 - month1;
-  }
-
-  if (day2 >= day1) {
-    day3 = day2 - day1;
-  } else {
+  if (day3 < 0) {
     month3--;
-    day3 = getDaysInMonth(year1, month1) + day2 - day1;
+    day3 += getDaysInMonth(today.getFullYear(), today.getMonth());
   }
+
   if (month3 < 0) {
-    month3 = 11;
     year3--;
+    month3 += 12;
   }
-  result.innerHTML = `You are <span>${year3}</span>  years, <span>${month3}</span>  months and <span>${day3}</span> days old`;
+
+  result.innerHTML = `You are <span>${year3}</span> years, <span>${month3}</span> months and <span>${day3}</span> days old`;
 }
 
 function getDaysInMonth(year, month) {
-  return new Date(year, month, 0).getDate();
+  return new Date(year, month + 1, 0).getDate();
 }
