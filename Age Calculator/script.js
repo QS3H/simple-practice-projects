a807a70f400b641c20b9a82d791d57bbbbd2e44d
shawky <mastershawky2017@gmail.com>
2025-01-28 21:13:11 +0200

    change style

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,36 +1,44 @@
-const btnEl = document.getElementById("btn");
-const birthdayEl = document.getElementById("birthday");
-const resultEl = document.getElementById("result");
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
 
 function calculateAge() {
-  const birthdayValue = birthdayEl.value;
-  if (birthdayValue === "") {
-    alert("Please enter your birthdate");
+  let birthDate = new Date(userInput.value);
+
+  let day1 = birthDate.getDate();
+  let month1 = birthDate.getMonth() + 1;
+  let year1 = birthDate.getFullYear();
+
+  let today = new Date();
+
+  let day2 = today.getDate();
+  let month2 = today.getMonth() + 1;
+  let year2 = today.getFullYear();
+
+  let day3, month3, year3;
+
+  year3 = year2 - year1;
+
+  if (month2 >= month1) {
+    month3 = month2 - month1;
   } else {
-    const age = getAge(birthdayValue);
-    document.addEventListener(
-      "click",
-      resultEl.innerText = `Your age is ${age} ${
-        age > 1 ? "years" : "year"
-      } old`
-    );
-  }
-}
-
-function getAge(birthdayValue) {
-  const currentDate = new Date();
-  const birthdayDate = new Date(birthdayValue);
-  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-  const month = currentDate.getMonth() - birthdayDate.getMonth();
-
-  if (
-    month < 0 ||
-    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-  ) {
-    age--;
+    year3--;
+    month3 = 12 + month2 - month1;
   }
 
-  return age;
+  if (day2 >= day1) {
+    day3 = day2 - day1;
+  } else {
+    month3--;
+    day3 = getDaysInMonth(year1, month1) + day2 - day1;
+  }
+  if (month3 < 0) {
+    month3 = 11;
+    year3--;
+  }
+  result.innerHTML = `You are <span>${year3}</span>  years, <span>${month3}</span>  months and <span>${day3}</span> days old`;
 }
 
-btnEl.addEventListener("click", calculateAge);
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
